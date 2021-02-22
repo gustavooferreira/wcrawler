0f5cfd034a1dfb14c4e789847d0d555dcea9c623
Gustavo Ferreira <gustavo.ferreira@sky.uk>
2021-02-22 02:40:27 +0000

    Added template for graph.html

--- pkg/core/graph/gen.go
+++ pkg/core/graph/gen.go
@@ -1,4 +1,7 @@
-package core
+package graph
+
+//go:embed "graph.tmpl"
+var htmlTemplate string
 
 // GraphGenerator generated an HTML file with the data pulled from the file.
 // It uses cytoscape.js library to render the graph in HTML.
